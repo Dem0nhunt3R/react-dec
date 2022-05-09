5b64ce55d48257e0c6d828c98d57caee7d35d4bc
d3m0nhunt3r <d3m0nhunt3r92@gmail.com>
2022-05-09 16:11:00 +0300

    lesson 5 hw 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
+import {useReducer} from "react";
+
+const init = (initCount) => {
+    return {counter1: initCount, counter2: initCount, counter3: initCount};
+}
+
+const reducer = (state, action) => {
+    const {type,payload} = action;
+
+    switch (type) {
+        case 'counter1':
+            return {...state,counter1:state.counter1+payload};
+        case 'counter2':
+            return {...state,counter2:state.counter2+payload};
+        case 'counter3':
+            return {...state,counter3:state.counter3+payload};
+        case 'reset1':
+            return {...state, counter1:0};
+        case 'reset2':
+            return {...state, counter2:0};
+        case 'reset3':
+            return {...state, counter3:0};
+        default:
+            console.log('unknown type')
+            return state;
+    }
+}
+
 function App() {
+    const [state, dispatch] = useReducer(reducer, 0, init);
 
     return (
         <div>
-            Hello
+            <div>
+                <div>count 1 = {state.counter1}</div>
+                <button onClick={() => dispatch({type: 'counter1',payload:1})}>inc</button>
+                <button onClick={() => dispatch({type: 'counter1',payload:-1})}>dec</button>
+                <button onClick={() => dispatch({type: 'reset1'})}>reset</button>
+            </div>
+            <div>
+                <div>count 2 = {state.counter2}</div>
+                <button onClick={() => dispatch({type: 'counter2',payload:1})}>inc</button>
+                <button onClick={() => dispatch({type: 'counter2',payload:-1})}>dec</button>
+                <button onClick={() => dispatch({type: 'reset2'})}>reset</button>
+            </div>
+            <div>
+                <div>count 3 = {state.counter3}</div>
+                <button onClick={() => dispatch({type: 'counter3',payload:1})}>inc</button>
+                <button onClick={() => dispatch({type: 'counter3',payload:-1})}>dec</button>
+                <button onClick={() => dispatch({type: 'reset3'})}>reset</button>
+            </div>
         </div>
     );
 }
